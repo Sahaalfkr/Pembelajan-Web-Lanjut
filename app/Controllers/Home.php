e7f6d02c714ce870015583bf8746882950d1ad1a
Sahaalfkr <sahaalfkr@gmail.com>
2026-05-25 04:54:22 +0700

    Fitur read data dan validasi

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ProductModel;
 
 class Home extends BaseController
-{
-    public function index(): string
+{   
+    protected $productModel;
+    public function __construct()
     {
-        return view('v_home');
+        $this->productModel = new ProductModel();
+    }
+    public function index()
+    {
+        return view('v_home', [
+	    'products' => $this->productModel->findAll()
+    ]);
     }
 }
