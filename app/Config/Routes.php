92c0c228bfcd89258bb8f533f084873b0b011445
Sahaalfkr <sahaalfkr@gmail.com>
2026-06-01 22:08:49 +0700

    Add CRUD feature

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,6 +14,12 @@ $routes->get('logout', 'AuthController::logout');
 
 // Route untuk halaman profil (protected dengan filter auth)
 $routes->get('profil', 'ProfileController::index', ['filter' => 'auth']);
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
 
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
