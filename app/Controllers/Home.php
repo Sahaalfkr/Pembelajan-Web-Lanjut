74339623043989902867f24142d48c79d6722152
Sahaalfkr <sahaalfkr@gmail.com>
2026-06-08 20:20:26 +0700

    Capstone-9-Library

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
 {   
     protected $productModel;
     public function __construct()
-    {
+    {   helper(['number', 'form']);
         $this->productModel = new ProductModel();
     }
     public function index()
